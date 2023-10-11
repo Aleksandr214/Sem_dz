90cea76a57ad358f51aca5e1ec2ec0daad294017
Aleksandr214 <aleksandrkocurov2144@gmail.com>
2023-10-11 20:38:37 +0400

    сохронил

---
+++ dz_5/Program.cs
@@ -0,0 +1,11 @@
+﻿//написать прогграмму,на входе 3х значное число и на выходе показывает 2-ю цифру этого числа.
+Console.Write("Введите трех значное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number>999||number<100)
+{
+    Console.WriteLine("Вы ввели не трех значное число: ");
+}
+else
+{
+    Console.WriteLine(number % 100/10); 
+}
--- dz_5/obj/Debug/net6.0/dz_5.AssemblyInfo.cs
+++ dz_5/obj/Debug/net6.0/dz_5.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("seminar_dz")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("dz_5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("seminar_dz")]
-[assembly: System.Reflection.AssemblyTitleAttribute("seminar_dz")]
+[assembly: System.Reflection.AssemblyProductAttribute("dz_5")]
+[assembly: System.Reflection.AssemblyTitleAttribute("dz_5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
